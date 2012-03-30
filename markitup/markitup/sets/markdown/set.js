5963643f26dabf2271604560e74b68462cdef793
Marko Elezovic <marko@element.hr>
2012-03-30 10:40:30 +0200

    Fancybox images, multi markdown markitup customization

--- markitup/markitup/sets/markdown/set.js
+++ markitup/markitup/sets/markdown/set.js
@@ -14,12 +14,12 @@ mySettings = {
 	previewParserPath:	'',
 	onShiftEnter:		{keepDefault:false, openWith:'\n\n'},
 	markupSet: [
-		{name:'First Level Heading', key:'1', placeHolder:'Your title here...', closeWith:function(markItUp) { return miu.markdownTitle(markItUp, '=') } },
-		{name:'Second Level Heading', key:'2', placeHolder:'Your title here...', closeWith:function(markItUp) { return miu.markdownTitle(markItUp, '-') } },
-		{name:'Heading 3', key:'3', openWith:'### ', placeHolder:'Your title here...' },
-		{name:'Heading 4', key:'4', openWith:'#### ', placeHolder:'Your title here...' },
-		{name:'Heading 5', key:'5', openWith:'##### ', placeHolder:'Your title here...' },
-		{name:'Heading 6', key:'6', openWith:'###### ', placeHolder:'Your title here...' },
+		{name:'Heading 1', key:'1', openWith:'## ', placeHolder:'Your title here...' },
+		{name:'Heading 2', key:'2', openWith:'### ', placeHolder:'Your title here...' },
+		{name:'Heading 3', key:'3', openWith:'#### ', placeHolder:'Your title here...' },
+		{name:'Heading 4', key:'4', openWith:'##### ', placeHolder:'Your title here...' },
+		{name:'Heading 5', key:'5', openWith:'###### ', placeHolder:'Your title here...' },
+		{name:'Heading 6', key:'6', openWith:'####### ', placeHolder:'Your title here...' },
 		{separator:'---------------' },		
 		{name:'Bold', key:'B', openWith:'**', closeWith:'**'},
 		{name:'Italic', key:'I', openWith:'_', closeWith:'_'},
@@ -33,9 +33,7 @@ mySettings = {
 		{name:'Link', key:'L', openWith:'[', closeWith:']([![Url:!:http://]!] "[![Title]!]")', placeHolder:'Your text to link here...' },
 		{separator:'---------------'},	
 		{name:'Quotes', openWith:'> '},
-		{name:'Code Block / Code', openWith:'(!(\t|!|`)!)', closeWith:'(!(`)!)'},
-		{separator:'---------------'},
-		{name:'Preview', call:'preview', className:"preview"}
+		{name:'Code Block / Code', openWith:'(!(\t|!|`)!)', closeWith:'(!(`)!)'}
 	]
 }
 
