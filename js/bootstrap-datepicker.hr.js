8f2c03079811134eb6420a3fabccf0c2ab9b1d45
Neven Falica <nfalica@element.hr>
2012-04-17 18:00:33 +0200

    Bootstrap Datepicker added, Bootstrap tweaked accordingly.

---
+++ js/bootstrap-datepicker.hr.js
@@ -0,0 +1,13 @@
+/**
+ * Croatian translation for bootstrap-datepicker
+ * Neven Falica <neven@falica.net>
+ */
+;(function($){
+  $.fn.datepicker.dates['hr'] = {
+    days: ["Nedjelja", "Ponedjeljak", "Utorak", "Srijeda", "Četvrtak", "Petak", "Subota", "Nedjelja"],
+    daysShort: ["Ned", "Pon", "Uto", "Sri", "Čet", "Pet", "Sub", "Ned"],
+    daysMin: ["N", "P", "U", "S", "Č", "P", "S", "N"],
+    months: ["Siječanj", "Veljača", "Ožujak", "Travanj", "Svibanj", "Lipanj", "Srpanj", "Kolovoz", "Rujan", "Listopad", "Studeni", "Prosinac"],
+    monthsShort: ["Sij", "Velj", "Ožu", "Tra", "Svi", "Lip", "Srp", "Kol", "Ruj", "Lis", "Stu", "Pro"]
+  };
+}(jQuery));
