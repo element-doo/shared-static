de1907955ed980c99552e5b96d83e1b5a52b84c8
Neven Falica <nfalica@element.hr>
2012-04-25 17:20:03 +0200

    fancyBox updated to 2.0.6, added experimental I18N support. bxSlider lost unneeded +x flags.

---
+++ js/jquery.fancybox.hr.js
@@ -0,0 +1,12 @@
+/**
+ * Croatian translation for jQuery fancyBox
+ * Neven Falica <neven@falica.net>
+ */
+(function($){
+  $.fancybox.defaults.texts = {
+    next: 'Slijedeće',
+    prev: 'Prethodno',
+    close: 'Zatvori',
+    error: 'Sadržaj nije pronađen.<br/>Molimo, probajte kasnije.'
+  };
+}(jQuery));
