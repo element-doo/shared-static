7013517bed95f5414d87ca0b364d452e79bf0d41
Neven Falica <nfalica@element.hr>
2012-05-05 18:44:05 +0200

    markItUp plugin translated.

--- markitup/markitup/sets/markdown/set.js
+++ markitup/markitup/sets/markdown/set.js
@@ -11,40 +11,47 @@
 // Feel free to add more tags
 // -------------------------------------------------------------------
 mySettings = {
-	previewParserPath:	'',
-	onShiftEnter:		{keepDefault:false, openWith:'\n\n'},
-	markupSet: [
-		{name:'Heading 1', key:'1', openWith:'## ', placeHolder:'Your title here...' },
-		{name:'Heading 2', key:'2', openWith:'### ', placeHolder:'Your title here...' },
-		{name:'Heading 3', key:'3', openWith:'#### ', placeHolder:'Your title here...' },
-		{name:'Heading 4', key:'4', openWith:'##### ', placeHolder:'Your title here...' },
-		{name:'Heading 5', key:'5', openWith:'###### ', placeHolder:'Your title here...' },
-		{name:'Heading 6', key:'6', openWith:'####### ', placeHolder:'Your title here...' },
-		{separator:'---------------' },		
-		{name:'Bold', key:'B', openWith:'**', closeWith:'**'},
-		{name:'Italic', key:'I', openWith:'_', closeWith:'_'},
-		{separator:'---------------' },
-		{name:'Bulleted List', openWith:'- ' },
-		{name:'Numeric List', openWith:function(markItUp) {
-			return markItUp.line+'. ';
-		}},
-		{separator:'---------------' },
-		{name:'Picture', key:'P', replaceWith:'![[![Alternative text]!]]([![Url:!:http://]!] "[![Title]!]")'},
-		{name:'Link', key:'L', openWith:'[', closeWith:']([![Url:!:http://]!] "[![Title]!]")', placeHolder:'Your text to link here...' },
-		{separator:'---------------'},	
-		{name:'Quotes', openWith:'> '},
-		{name:'Code Block / Code', openWith:'(!(\t|!|`)!)', closeWith:'(!(`)!)'}
-	]
+  previewParserPath:  '',
+  onShiftEnter:    {keepDefault:false, openWith:'\n\n'},
+  markupSet: [
+    {name:'Naslov 1', key:'1', openWith:'## ', placeHolder:'Naslov...' },
+    {name:'Naslov 2', key:'2', openWith:'### ', placeHolder:'Naslov...' },
+    {name:'Naslov 3', key:'3', openWith:'#### ', placeHolder:'Naslov...' },
+    {name:'Naslov 4', key:'4', openWith:'##### ', placeHolder:'Naslov...' },
+    {name:'Naslov 5', key:'5', openWith:'###### ', placeHolder:'Naslov...' },
+    {name:'Naslov 6', key:'6', openWith:'####### ', placeHolder:'Naslov...' },
+    {separator:'---------------' },
+    {name:'Bold', key:'B', openWith:'**', closeWith:'**'},
+    {name:'Italic', key:'I', openWith:'_', closeWith:'_'},
+    {separator:'---------------' },
+    {name:'Centriraj naslov', key:'C', replaceWith:function(markItUp){
+      return '[CENTER]\r\n' + markItUp.selection + '\r\n[/CENTER]\r\n';
+    }},
+    {separator:'---------------' },
+    {name:'Novi red', openWith:'[br]\r\n', className:'pilcrow' },
+    {separator:'---------------' },
+    {name:'Lista s točkama', openWith:'- ' },
+    {name:'Lista s brojevima', openWith:function(markItUp) {
+      return markItUp.line+'. ';
+    }},
+    {separator:'---------------' },
+    {name:'Slika (s eksternog servera)', key:'P', replaceWith:'![[![Opisni tekst]!]]([![Url:!:http://]!] "[![Title]!]")'},
+    {name:'Link', key:'L', openWith:'[', closeWith:']([![Url:!:http://]!] "[![Title]!]")', placeHolder:'Opis...' },
+    {separator:'---------------'},
+    {name:'Grb RH', openWith:'[RH-GRB]\r\n', className:'GrbRH' }
+    //{name:'Citat', openWith:'> '},
+    //{name:'Code Block / Code', openWith:'(!(\t|!|`)!)', closeWith:'(!(`)!)'}
+  ]
 }
 
 // mIu nameSpace to avoid conflict.
 miu = {
-	markdownTitle: function(markItUp, char) {
-		heading = '';
-		n = $.trim(markItUp.selection||markItUp.placeHolder).length;
-		for(i = 0; i < n; i++) {
-			heading += char;
-		}
-		return '\n'+heading;
-	}
+  markdownTitle: function(markItUp, char) {
+    heading = '';
+    n = $.trim(markItUp.selection||markItUp.placeHolder).length;
+    for(i = 0; i < n; i++) {
+      heading += char;
+    }
+    return '\n'+heading;
+  }
 }
