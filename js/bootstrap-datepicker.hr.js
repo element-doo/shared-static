b88f63aff69065e1351cdcb925cdbec4a224fd04
Marko Elezović <marko@element.hr>
2013-08-26 13:32:47 +0200

    Corrections of the Bootstrap DatePicker Croatian translation.

--- js/bootstrap-datepicker.hr.js
+++ js/bootstrap-datepicker.hr.js
@@ -1,15 +1,13 @@
 /**
- * Croatian localisation
+ * Croatian translation for bootstrap-datepicker
+ * Neven Falica <neven@falica.net>
  */
 ;(function($){
-	$.fn.datepicker.dates['hr'] = {
-		days: ["Nedjelja", "Ponedjelja", "Utorak", "Srijeda", "Četrtak", "Petak", "Subota", "Nedjelja"],
-		daysShort: ["Ned", "Pon", "Uto", "Srr", "Čet", "Pet", "Sub", "Ned"],
-		daysMin: ["Ne", "Po", "Ut", "Sr", "Če", "Pe", "Su", "Ne"],
-		months: ["Siječanj", "Veljača", "Ožujak", "Travanj", "Svibanj", "Lipanj", "Srpanj", "Kolovoz", "Rujan", "Listopad", "Studeni", "Prosinac"],
-		monthsShort: ["Sije", "Velj", "Ožu", "Tra", "Svi", "Lip", "Jul", "Kol", "Ruj", "Lis", "Stu", "Pro"],
-		today: "Danas",
-		weekStart: 1,
-		format: "dd.mm.yyyy."
-	};
+  $.fn.datepicker.dates['hr'] = {
+    days: ["Nedjelja", "Ponedjeljak", "Utorak", "Srijeda", "Četvrtak", "Petak", "Subota", "Nedjelja"],
+    daysShort: ["Ned", "Pon", "Uto", "Sri", "Čet", "Pet", "Sub", "Ned"],
+    daysMin: ["N", "P", "U", "S", "Č", "P", "S", "N"],
+    months: ["Siječanj", "Veljača", "Ožujak", "Travanj", "Svibanj", "Lipanj", "Srpanj", "Kolovoz", "Rujan", "Listopad", "Studeni", "Prosinac"],
+    monthsShort: ["Sij", "Velj", "Ožu", "Tra", "Svi", "Lip", "Srp", "Kol", "Ruj", "Lis", "Stu", "Pro"]
+  };
 }(jQuery));
